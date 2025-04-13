de54fe7bf2f7bd89a439bcaa0ad9f14f8950051a
RyuuKun-00 <roman.semenov_00@mail.ru>
2025-04-13 11:37:08 +0300

    save

--- backend/backend.Core/Abstractions/IContactRepository.cs
+++ backend/backend.Core/Abstractions/IContactRepository.cs
@@ -7,7 +7,6 @@ namespace backend.DataAccess.Repositories
         Task<Guid> Create(Contact contact);
         Task<Guid> Delete(Guid id);
         Task<List<Contact>> Get();
-
         Task<List<Contact>> Search(string searchString);
         Task<Guid> Update(Guid id, string name, string number, string description);
     }
